9e181739ae6578de69c1c5583680b57b319862e9
Murali-Manohar31 <muralikishan43@gmail.com>
2026-04-02 23:18:34 +0530

    added calculator GUI

--- calculator_gui.py
+++ calculator_gui.py
@@ -5,6 +5,13 @@ import easyocr #Reads the text from file
 import cv2 #image processing 
 from sympy import sympify #solves math
 
+root=tk.Tk()
+root.title("Calculator")
+
+panel=tk.Label(root)
+panel.pack()
+output_text=tk.Text(root,height=15,width=50)
+output_text.pack()
 #initialize ocr
 reader= easyocr.Reader(['en'],gpu='false')
 
@@ -30,5 +37,22 @@ def upload_image():
         img=Image.open(file_path)#open image
         img=img.resize((250,250))#image reszie
         img=ImageTk.PhotoImage(img)#convert Tkinter display
+        #displays image in GUI
         panel.config(image=img)
         panel.image=img
+        lines= extract_image(file_path)#calls OCR 
+        
+        output_text.delete("1.0",tk.END)        
+        output_text.insert(tk.END, "Detected:\n")
+        for line in lines:
+            output_text.insert(tk.END,line+ "\n")#displays OCR text
+        output_text.insert(tk.END, "\nResults:\n")
+        
+        for line in lines:
+            result = solve_expression(line)
+            output_text.insert(tk.END,f"{line}={result}\n")#calculates each line and shows result
+            
+#GUI Window
+root=tk.Tk()
+root.title("Handwritten Calculator")
+root.geometry("500x600")
