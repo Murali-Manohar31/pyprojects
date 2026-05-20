8a0dcd15e54320b6895c1fd3c28f4bd1eecd7217
Murali-Manohar31 <muralikishan43@gmail.com>
2026-05-20 20:01:40 +0530

    updated with new changes

--- obj_detector.py
+++ obj_detector.py
@@ -9,3 +9,14 @@ cap=cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Cannnot access the Camera")
     exit()
+    
+prev_time=0
+while True:
+    ret,frame=cap.read()
+    
+    if not ret:
+        print("failed to grab frame")
+        break
+#YOLO Detection
+results=model(frame)
+annotated_frame = results[0].plot()
