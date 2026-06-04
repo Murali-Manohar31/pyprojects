193f39362d8dd81b77c54ec9608d2af59fe99d36
Murali-Manohar31 <muralikishan43@gmail.com>
2026-06-04 19:53:34 +0530

    updated

--- obj_detector.py
+++ obj_detector.py
@@ -18,14 +18,22 @@ while True:
         print("failed to grab frame")
         break
 #YOLO Detection
-results=model(frame)
-annotated_frame = results[0].plot()
+    results=model(frame)
+    annotated_frame = results[0].plot()
 
-current_time=time.time()
-fps=1/(current_time-prev_time)
-prev_time=current_time
+    current_time=time.time()
+    fps=1/(current_time-prev_time)
+    prev_time=current_time
 
-cv2.putText(
-    annotated_frame,
-    f"FPS: {int(fps)}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2   
+    cv2.putText(
+        annotated_frame,
+        f"FPS: {int(fps)}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2   
 )
+
+    cv2.imshow("Real-Time Object Detector ",annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
